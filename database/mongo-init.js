1ef5d38846616b36f3c25a3d5cbe1ed6c8be700e
Pablo Lopes <>
2019-06-25 17:36:46 -0300

    Changed the schema of data for mongo

--- database/mongo-init.js
+++ database/mongo-init.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 /** Init file to create user, database and schema**/
-conn = new Mongo();
-db = conn.getDB("goodreadsbot");
+db.auth("admin", "123@Abc");
 
 db.createUser(
     {
@@ -13,17 +12,69 @@ db.createUser(
 
 /** create users collection with a schema validator **/
 /* db.runCommand( {
-    collMod: "contacts", */
+    collMod: "users", */
 db.createCollection("users", {
     validator: {
         $jsonSchema: {
             bsonType: "object",
-            required: ["facebookId"],
+            required: ["messengerId", "firstName", "currentState"],
             additionalProperties: true,
             properties: {
-                facebookId: {
+                messengerId: {
                     bsonType: "string",
                     description: "string required"
+                },
+                firstName: {
+                    bsonType: "string",
+                    description: "string required"
+                },
+                currentState: {
+                    enum: ["CHOOSING_TYPE_SEARCH", "SEARCHING_BY_ID", "SEARCHING_BY_TITLE", "CHOOSING_BOOK", "VIEWING BOOK", null],
+                    description: "string required"
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("books", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["goodreadsId", "title", "author", "smallImage"],
+            additionalProperties: true,
+            properties: {
+                goodreadsId: {
+                    bsonType: "string",
+                    description: "string required"
+                },
+                title: {
+                    bsonType: "string",
+                    description: "string required"
+                },
+                author: {
+                    bsonType: "string",
+                    description: "string required"
+                },
+                smallImage: {
+                    bsonType: "string",
+                    description: "string required"
+                },
+                isbn: {
+                    bsonType: ["string", "null"],
+                    description: "string not required"
+                },
+                reviews: {
+                    bsonType: "array",
+                    description: "array of the book's reviews. not required",
+                    minItems: 0,
+                    items: {
+                        bsonType: "string"
+                    }
+                },
+                shouldBy: {
+                    enum: [true, false],
+                    description: "information if user should by or not. not required"
                 }
             }
         }
